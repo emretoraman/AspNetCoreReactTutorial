34e7e1cfb49f23b72026b17b0356e7a9faf6bd69
Emre Toraman <emretoraman@gmail.com>
2022-02-02 11:48:53 +0300

    Posts Table

--- reactclient/src/index.js
+++ reactclient/src/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import ReactDOM from 'react-dom';
 import App from './App';
 
